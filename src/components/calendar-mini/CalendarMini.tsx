0889966141197e2c8b0893e2adc23efc44b3eea7
Daniel Sprohar <daniel.sprohar@gmail.com>
2024-12-05 13:42:10 -0600

    Refactor App component to use CalendarMini and add CalendarMiniBody component

---
+++ src/components/calendar-mini/CalendarMini.tsx
@@ -0,0 +1,9 @@
+// interface CalendarMiniProps {
+//   children?: React.ReactNode
+// }
+
+import CalendarMiniBody from './CalendarMiniBody'
+
+export default function CalendarMini() {
+  return <CalendarMiniBody />
+}

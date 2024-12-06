d743ede60ae014de78fda0899715482a6e539efc
Daniel Sprohar <daniel.sprohar@gmail.com>
2024-12-06 14:01:39 -0600

    fix: correct SVG attribute casing in CalendarMiniHeader component

--- src/components/calendar/mini/CalendarMiniHeader.tsx
+++ src/components/calendar/mini/CalendarMiniHeader.tsx
@@ -20,13 +20,13 @@ export default function CalendarMiniHeader({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="size-4"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.75 19.5 8.25 12l7.5-7.5"
             />
           </svg>
@@ -42,13 +42,13 @@ export default function CalendarMiniHeader({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="size-4"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="m8.25 4.5 7.5 7.5-7.5 7.5"
             />
           </svg>
